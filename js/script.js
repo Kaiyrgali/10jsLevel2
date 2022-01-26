f106a065b547cd0208e9deab619d5d31172ae85a
Kaiyrgali <kairgalib@ya.ru>
2022-01-26 21:37:01 +0500

    Level2 JS

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 "use strict"
 
-let firstMessage = "Hello World";
-alert (firstMessage);
-
-let userLogin = true;
-let userAge = 123;
-let userPassword = userLogin + userAge;
-console.log(userPassword);
-// Bollean значение "true" численно в математических выражениях преобразуется в единицу, "false" - в ноль
-
-let login = "";
-let secondMessage = (login == 'Сотрудник') ? 'Привет' :
-    (login == 'Директор') ? 'Здравствуйте' :
-    (login == '') ? 'Нет логина' : '' ;
-alert (secondMessage);
-
-let age = prompt('Введите свой возраст от 18 до 80', 18) ;
-let thirdMessage = ( age >= 18 && age <= 80 ) ? 'Все нормально' : 'Возраст не верен' ;
-alert (thirdMessage);
-
-for (let index = 1; index < 10; index++) {
-    if ( Number.isInteger (index / 2) ) continue ;
-    console.log (index) ;
-}
-
-let index2 = 10
-while (index2 < 20) {
-    if ( index2 % 2 != 0) console.log (index2) ;
-    index2 ++ ;
-}
-
-function max(a, b) {
-  if ( a > b ) { return a;
- } else if ( b > a ) { return b;
-    }   else { return 'a и b равны';
-        }
+const database = {
+    one: 1,
+    two: 2,
+    three: "anyString",
 };
-console.log (max (100,20));
+function multiplyNumeric (obj) {
+    for (let key in obj) {
+        if ( typeof (obj[key]) === 'number' ) {
+            obj[key] = obj[key] * 4 ;
+        } ;
+    };
+};
+console.log (database); // original object
+multiplyNumeric (database) ;
+console.log (database); //changed object
+
+const calculator = {
+    read:  function() {
+        let getNum1 = +prompt ("Введите значение №1", 2) ;
+        this.num1 = getNum1 ;
+        let getNum2 = +prompt ("Введите значение №2", 5) ;
+        this.num2 = getNum2 ;
+    },
+    sum: function() {return this.num1 + this.num2 },
+    mul: function() {return this.num1 * this.num2 },
+}
+calculator.read ();
+console.log (calculator.sum ()) ;
+console.log (calculator.mul ()) ;
+
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};
+let json = JSON.stringify (user);
+let userNew = JSON.parse (json);
+console.log (userNew);
